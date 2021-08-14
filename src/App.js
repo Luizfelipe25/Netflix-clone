e4ea6dc8577b5f51f9f69ea72d25609ef3312827
Luiz Felipe Farias Da Silva Santos <luiz.farias.cpv@gmail.com>
2021-08-14 18:32:29 -0300

    FecthingAPI and getting the necessary data for the page

--- src/App.js
+++ src/App.js
@@ -1,5 +1,16 @@
-import React from 'react'
+/* eslint-disable import/no-anonymous-default-export */
+import React, {useEffect,useState} from 'react'
+import Tmdb from './tmdb'
 
 export default () => {
+  const [movieList,setMovieList] = useState([])
+
+  useEffect(()=>{
+    const loadAll = async () => {
+      let list = await Tmdb.getHomeList();
+      setMovieList(list);
+    }
+    loadAll();
+  }, [])
   return (<div>olá mundo</div>)
-}
+} 
