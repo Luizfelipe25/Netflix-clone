3e1749dc3ccf35806535418921ac5a549f5e46d8
Luiz Felipe Farias Da Silva Santos <luiz.farias.cpv@gmail.com>
2021-08-14 19:03:56 -0300

    starting customize

--- src/App.js
+++ src/App.js
@@ -1,16 +1,25 @@
 /* eslint-disable import/no-anonymous-default-export */
-import React, {useEffect,useState} from 'react'
-import Tmdb from './tmdb'
+import React, { useEffect, useState } from "react";
+import Tmdb from "./tmdb";
+import MovieRow from "./components/MovieRow";
 
 export default () => {
-  const [movieList,setMovieList] = useState([])
+  const [movieList, setMovieList] = useState([]);
 
-  useEffect(()=>{
+  useEffect(() => {
     const loadAll = async () => {
       let list = await Tmdb.getHomeList();
       setMovieList(list);
-    }
+    };
     loadAll();
-  }, [])
-  return (<div>olá mundo</div>)
-} 
+  }, []);
+  return (
+    <div className="page">
+      <section className="lists">
+        {movieList.map((item, key) => (
+          <MovieRow key={key} title={item.title} items={item.items} />
+        ))}
+      </section>
+    </div>
+  );
+};
