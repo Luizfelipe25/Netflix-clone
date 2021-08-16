b49196d26dad19593afd3a52a8241246020ff800
Luiz Felipe Farias Da Silva Santos <luiz.farias.cpv@gmail.com>
2021-08-16 01:45:07 -0300

    Styles

--- src/App.js
+++ src/App.js
@@ -2,19 +2,29 @@
 import React, { useEffect, useState } from "react";
 import Tmdb from "./tmdb";
 import MovieRow from "./components/MovieRow";
+import "./App.css";
+import FeatureMovie from "./components/FeatureMovie";
 
 export default () => {
   const [movieList, setMovieList] = useState([]);
+  const [featureData, setFeatureData] = useState([null]);
 
   useEffect(() => {
     const loadAll = async () => {
       let list = await Tmdb.getHomeList();
       setMovieList(list);
+
+      let originals = list.filter((i) => i.slug === "originals");
+      let randomChosen = Math.floor(
+        Math.random() * (originals[0].items.results.length - 1)
+      );
+      let chosen = originals[0].items.results[randomChosen];
     };
     loadAll();
   }, []);
   return (
     <div className="page">
+      {featureData && <FeatureMovie item={featureData} />}
       <section className="lists">
         {movieList.map((item, key) => (
           <MovieRow key={key} title={item.title} items={item.items} />
